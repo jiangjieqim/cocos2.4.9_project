8cf699baea5ffb3ee5f6055377c6b2605b933700
jiangjieqim@hotmai.com <jiangjieqim@hotmai.com>
2025-05-26 22:32:21 +0800

    .

--- cmd/build_wx.bat
+++ cmd/build_wx.bat
@@ -1,4 +1,9 @@
 set workspaceFolder=%1
+set platform=%2
+@REM wechatgame
+
+if "%platform%" == "" ( echo please set platform! & exit )
+
 cd %workspaceFolder%
 echo %workspaceFolder%
 cd ..\..\..\zzqx\tools\nodeJsTool\qatools
@@ -7,7 +12,7 @@ set QATOOLS=%cd%
 
 cd %QATOOLS%
 
-node cc_compile.js --w %workspaceFolder% --remote "http://127.0.0.1:8001/github/cocos2.4.9_project/test1/build/wechatgame/"
+node cc_compile.js --w %workspaceFolder% --remote "http://127.0.0.1:8001/github/cocos2.4.9_project/test1/build/%platform%/" --platform %workspaceFolder%\settings\%platform%.json
 
-node cc_compile_clear.js --w %workspaceFolder%
+node cc_compile_clear.js --path %workspaceFolder%/build/%platform%
 
