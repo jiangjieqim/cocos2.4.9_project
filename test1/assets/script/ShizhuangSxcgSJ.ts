bcc57118bb5a8b4e5a6a2793cd69054b130ab2c1
admin <jiangjieqim@hotmail.com>
2025-07-15 14:15:01 +0800

    .

--- test1/assets/script/ShizhuangSxcgSJ.ts
+++ test1/assets/script/ShizhuangSxcgSJ.ts
@@ -4,8 +4,9 @@ const { ccclass } = cc._decorator;
 export class ShizhuangSxcgSJ extends cc.Component {
     onLoad() {
         const spine: sp.Skeleton = this.node.getComponent(sp.Skeleton);
+        console.log("onLoad..."+Date.now()/1000);
         spine.setEventListener((trackEntry, event) => {
-            cc.log(`event name:[${event.data.name}],event params:${event.stringValue}, ${event.intValue}, ${event.floatValue}`);
+            cc.log( `play.....${Date.now()/1000} event name:[${event.data.name}],event params:${event.stringValue}, ${event.intValue}, ${event.floatValue}`);
         });
         spine.setAnimation(0, "shizhuang_sxcg", false);
     }
